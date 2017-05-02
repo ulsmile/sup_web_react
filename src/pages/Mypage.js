2138955484f9914ed2eb2ac3bafad732a3d5190b
sivertigo <sg.tak22@gmail.com>
2017-05-02 17:38:55 +0900

    change the organization of this app

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -10,7 +10,7 @@ const MypageInfo = () => (
   <MuiThemeProvider>
     <div>
       <h1>マイページ</h1>
-      <Avatar src="src/images/ktomi.JPG" />
+      <Avatar src="http://res.cloudinary.com/dvudu2gsj/image/upload/v1493364948/DSC_0178_p3j1vm.jpg" />
       <h2>名前</h2>
       <SimpleTextField />
       <SimpleCheckbox />
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,48 @@
+const ExtractTextPlugin
+	= require('extract-text-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-  entry: "./src/App.js",
+	context: path.resolve(__dirname, 'src'),
+  entry: {
+		//"./src.App.js"
+		js: './entry.js',//changed name "App.js to entry.js"
+		css: './entry.css'
+	},
   output: {
-    path: __dirname,
-    filename: "bundle.js"
+    path: path.resolve(__dirname,'dist'),
+    filename: "bundle.[name]"
   },
+		plugins: [
+			new ExtractTextPlugin('bundle.css'),
+		],
+		devtool: 'source-map',
   devServer: {
-    port: 3002
-  },
+		contentBase: './dist/',
+    port: 8080,
+		inline: true,
+		historyApiFallback: true,},
   module: {
     loaders: [
-      { test: /\.js$/,  loader: "babel-loader" , exclude: /node_modules/ }
-    ]
+      { test: /\.js$/,  loader: "babel-loader" , exclude: /node_modules/ },
+			{
+				test: /\.css$/,
+			 	use: ExtractTextPlugin.extract(
+					{
+						fallback: "style-loader",
+						use: ['css-loader',{
+							loader: 'postcss-loader',
+							options:{
+								plugins: () => [
+									require('postcss-easy-import')({glob: true}),
+									]
+								},
+							},
+						]
+						}
+					),
+			}
+    ],
+			
   }
 }
